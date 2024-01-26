9abc3c7bb12f4ef2bb4899c669eb009d94d36942
Manish Shahi <manish.shahivishen@gmail.com>
2024-01-26 15:01:10 +0530

    Add files via upload
    landing page

---
+++ click-joy-media(1)/parts/footer.php
@@ -0,0 +1,48 @@
+<div class="container">
+    <div class="footer_box">
+        <div class="footer_box_main_content_box">
+            <div class="footer_box_main_content_item">
+                <a href="/" class="footer_main_logo"><img src="./assets/images/black-logo-cjm.svg" alt=""></a>
+                <p>With over two decades of expertise in expanding mVAS services, we aim to share this wealth of knowledge with your business!</p>
+            </div>
+            <div class="footer_box_main_content_mor_link_box">
+                <div class="footer_box_main_content_mor_link_item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">How it works</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Demo</a></li>
+                    </ul>
+                </div>
+                <div class="footer_box_main_content_mor_link_item">
+                    <h3>Expertise</h3>
+                    <ul>
+                        <li><a href="#">Google mVAS</a></li>
+                        <li><a href="#">Google CC Submit</a></li>
+                        <li><a href="#">Google UAC</a></li>
+                    </ul>
+                </div>
+                <div class="footer_box_main_content_mor_link_item">
+                    <h3>Contact</h3>
+                    <ul>
+                        <li><a href="tel:+91xxxxxxxxxx"><i class="fa-solid fa-phone"></i> +91-xxxxx xxxxx</a></li>
+                        <li><a href="mailto:info.clickjoymedia.com"><i class="fa-solid fa-envelope"></i> info.clickjoymedia.com</a></li>
+                        <li><a href="#"><i class="fa-solid fa-location-dot"></i> lorem ipsum dolor is met</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copy_right_main_footer_box">
+            <div class="copy_right_main_compny_box">
+                <p>Copyright &copy; <span id="copyright_year"><script>document.getElementById("copyright_year").textContent = new Date().getFullYear();</script></span> Click Joy Media.</p>
+            </div>
+            <div class="copy_right_main_media_icons_box">
+                <a href="#" class="copy_right_main_media_icon"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#" class="copy_right_main_media_icon"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#" class="copy_right_main_media_icon"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="#" class="copy_right_main_media_icon"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ click-joy-media(1)/parts/header.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="header_box">
+        <a href="/" class="header_logo"><img src="./assets/images/logo-cjm.svg" alt="Logo"></a>
+        <nav class="header_nav_items">
+            <a href="/" class="nav_logo"><img src="./assets/images/logo-cjm.svg" alt="Logo"></a>
+            <span class="cross_icon">&#10006;</span>
+            <ul>
+                <li><a href="/">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="./privacy-policy">PRIVACY POLICY</a></li>
+                <li><a href="./terms-and-condition">TERMS AND CONDITIONS</a></li>
+            </ul>
+            <a href="#" class="header_cta_btn contact_us_header_cta_btn">CONTACT US</a>
+            <a href="#" class="header_cta_btn joinusnow_header_cta_btn">JOIN US NOW</a>
+        </nav>
+        <div class="header_right_box">
+            <a href="#" class="header_cta_btn contact_us_header_cta_btn">CONTACT US</a>
+            <a href="#" class="header_cta_btn joinusnow_header_cta_btn">JOIN US NOW</a>
+        </div>
+        <span class="menu_btn"><i class="fa-solid fa-bars"></i></span>
+    </div>
+</div>
